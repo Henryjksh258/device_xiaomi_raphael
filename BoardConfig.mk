fa91d0fa47f2d2bff86ef1b34b644d3449724509
chandra <chandra.chaganti@hotmail.com>
2019-12-21 14:31:33 +0530

    raphael: Use aosp SurfaceFlinger
     * AOSP surfaceflinger has more smoother ui than that of CAF

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,6 +156,9 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/public \
     device/qcom/sepolicy/qva/public
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Treble
 BOARD_VNDK_VERSION := current
 PRODUCT_FULL_TREBLE_OVERRIDE := true
