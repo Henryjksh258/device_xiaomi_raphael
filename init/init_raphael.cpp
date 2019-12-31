ca549f933bcf6c20773989d2e87fa7436dc6b455
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-01 08:53:09 +0900

    raphael: init: remove duplicate prop override calls
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -39,8 +39,6 @@ void load_raphaelglobal() {
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
     property_override("ro.build.description", "raphael-user 10 QKQ1.190716.003 V10.4.6.0.QFKCNXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-    property_override("ro.control_privapp_permissions", "log");
 }
 
 void load_raphaelin() {
@@ -48,8 +46,6 @@ void load_raphaelin() {
     property_override("ro.build.product", "raphaelin");
     property_override("ro.product.device", "raphaelin");
     property_override("ro.build.description", "raphaelin-user 9 PKQ1.181121.001 V10.3.3.0.PFKINXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-    property_override("ro.control_privapp_permissions", "log");
 }
 
 void load_raphael() {
@@ -57,8 +53,6 @@ void load_raphael() {
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
     property_override("ro.build.description", "raphael-user 10 QKQ1.190716.003 V10.4.6.0.QFKCNXM release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-    property_override("ro.control_privapp_permissions", "log");
 }
 
 
@@ -76,6 +70,8 @@ void vendor_load_properties() {
     }
 
     property_override("ro.bluetooth.a2dp_offload.supported", "false");
+    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.control_privapp_permissions", "log");
     property_override("persist.bluetooth.a2dp_offload.disabled", "true");
     property_override("persist.bluetooth.bluetooth_audio_hal.disabled", "true");
 }
