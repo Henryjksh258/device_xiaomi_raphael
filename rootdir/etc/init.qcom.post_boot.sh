17c1ac256e6257e117bc5e6feafa418d713300bb
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-13 18:55:11 +0900

    raphael: init.qcom.post_boot.sh: use Pixel 4 cpusets config
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -4568,18 +4568,13 @@ case "$target" in
         echo 1 > /proc/sys/vm/watermark_scale_factor
 
 	# configure cpusets
-        echo 0-3 > /dev/cpuset/top-app/cpus
-        echo 1-2 > /dev/cpuset/audio-app/cpus
-        echo 0-3 > /dev/cpuset/foreground/cpus
-        echo 0-3 > /dev/cpuset/foreground/boost/cpus
-        echo 0-3 > /dev/cpuset/background/cpus
-        echo 0-3 > /dev/cpuset/system-background/cpus
-        echo 0-7 > /dev/cpuset/top-app/cpus
-        echo 0-7 > /dev/cpuset/foreground/cpus
-        echo 0-7 > /dev/cpuset/foreground/boost/cpus
-        echo 0-7 > /dev/cpuset/background/cpus
-        echo 0-7 > /dev/cpuset/system-background/cpus
-        echo 0-7 > /dev/cpuset/camera-daemon/cpus
+    echo 1-2 > /dev/cpuset/audio-app/cpus
+    echo 0-1 > /dev/cpuset/background/cpus
+    echo 0-7 > /dev/cpuset/camera-daemon/cpus
+    echo 0-3,5-6 > /dev/cpuset/foreground/cpus
+    echo 0-3 > /dev/cpuset/restricted/cpus
+    echo 0-3 > /dev/cpuset/system-background/cpus
+    echo 0-7 > /dev/cpuset/top-app/cpus
 
 	# Setup final blkio
 	# value for group_idle is us
